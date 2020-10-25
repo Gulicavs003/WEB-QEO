26af75f722ba297c3f6b0b5d0c53e71405fc189f
Gulicavs003 <giulianodamico2019@gmail.com>
2020-10-25 15:05:36 -0300

    General

---
+++ index.js
@@ -0,0 +1,35 @@
+function copiarAlPortapapeles(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+  }
+
+  let mostrarReloj=()=>{
+	let reloj=document.getElementById('reloj')
+	let fec_Datos=document.getElementById('fec_Datos')
+	let fecha =new Date();
+	let hora=fecha.getHours()
+	let minutos=fecha.getMinutes()
+	let segundos=fecha.getSeconds()
+	let mes=fecha.getMonth()
+	let dia=fecha.getDate()
+	let año=fecha.getFullYear()
+
+	let dias=['lunes','martes','miercoles','jueves','viernes','sabado','domingo']
+	let meses=['enero','febrero','marzo','abril','mayo','junnio','julio','agosto','septiembre','noviembre','diciembre']
+
+	mes=meses[mes]
+	let hr=(hora>12) ? hora-12 : hora
+	let am=(hora<12)? 'AM' :'PM'
+	if(hora<10){hora='0'+hora}
+	if(minutos<10){minutos='0'+ minutos}
+	if(segundos<10){segundos='0'+minutos}
+
+	reloj.textContent=`${hr}:${minutos}:${segundos}:${am}`
+	fec_Datos.textContent=`${dia} ${mes} del ${año}`
+   }
+
+   setInterval(mostrarReloj,1000)
---
+++ inicio.js
@@ -0,0 +1,18 @@
+function Login(){ 
+	var done=0; 
+	var usuario=document.login.usuario.value; 
+	var password=document.login.password.value; 
+	if (usuario=="1" && password=="1") { 
+	window.location="/index.html"; 
+	}
+    if (usuario=="Nanocavs" && password=="Monubarapisarda18") { 
+        window.location="/index.html"; 
+        } 
+    if (usuario=="pelado" && password=="pelado22gtav") { 
+            window.location="/index.html"; 
+    } 
+    if (usuario=="1" && password=="1") { 
+        window.location="/index.html"; 
+    }   
+	} 
+   
